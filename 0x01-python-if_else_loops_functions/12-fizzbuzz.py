e30bcbd9844187ca43cf3740d6324afab746a3e5
Fnan1996 <fnantedros09@gmail.com>
2022-09-20 10:03:13 +0000

    tasl

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,6 @@
- in range(1, 101):
+ #!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
         if (i % 5 == 0) and (i % 3 == 0):
             print('FizzBuzz ', end='')
         elif (i % 3 == 0):
